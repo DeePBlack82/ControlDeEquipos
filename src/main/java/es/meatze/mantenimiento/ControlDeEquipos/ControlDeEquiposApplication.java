c2edb62a6f5ca03c20c4f38a306b191f2eb88c91
Dani <dapabai@gmail.com>
2020-12-05 16:59:08 +0100

    Comienzo del proyecto

---
+++ src/main/java/es/meatze/mantenimiento/ControlDeEquipos/ControlDeEquiposApplication.java
@@ -0,0 +1,13 @@
+package es.meatze.mantenimiento.ControlDeEquipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeEquiposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/meatze/mantenimiento/ControlDeEquipos/ControlDeEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package es.meatze.mantenimiento.ControlDeEquipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
